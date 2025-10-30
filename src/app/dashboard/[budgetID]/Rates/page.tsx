782713fda3d5d95171cbd181fbf418e7c4cc88d5
theIntethesis <hayden@gnorth.net>
2025-10-30 11:18:54 -0700

    thsd'for

--- src/app/dashboard/[budgetID]/Rates/page.tsx
+++ src/app/dashboard/[budgetID]/Rates/page.tsx
@@ -8,9 +8,16 @@ export default function Page() {
     }
     return <main className="two">
         <div className='two-column'>
+            <label>[Student / Faculty Selector]</label>
+            <select>
+                <option></option>
+            </select>
             <Form action={onSubmit}>
                 <table>
                     <tbody>
+                        <tr>
+                            <td>[NAME CENTERED]</td>
+                        </tr>
                         <tr>
                             <td>
                                 Travel
