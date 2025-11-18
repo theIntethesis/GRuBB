065bb47cd6ec6214fa4d97530600754e09fa4eab
Julia <themusicalartist12@gmail.com>
2025-11-17 19:21:30 -0800

    hook up backend

--- src/app/dashboard/[budgetID]/Rates/layout.tsx
+++ src/app/dashboard/[budgetID]/Rates/layout.tsx
@@ -1,6 +1,7 @@
 "use server"
 import dbConnect from "@/lib/mongodb"
 import { Faculty, Individual, InstitutionalAccount, SalaryAccount, StudentAccount, Student } from "@/lib/models"
+import Link from "next/link";
 
 // Rates
 
@@ -12,54 +13,33 @@ export default async function Page({ params, children }) {
     await dbConnect()
     const { budgetID } = await params
 
-        // if this returns more than one institutional account then budgetID is not unique and something has fucked up.
-        const semesters = await InstitutionalAccount
-            .find({budgetID: budgetID})
-            .select(["semester", "inStateTuitionRate", "outOfStateTuitionRate", "tuitionIncrease", "facultyFBR", "studentFBR", "postDocFBR", "travelProfile", "overheadCharge"])
-            .lean()
+    // if this returns more than one institutional account then budgetID is not unique and something has fucked up.
+    const semesters = await InstitutionalAccount
+        .find({budgetID: budgetID})
+        .lean()
 
-        // console.log(studentAccIds)
-        // console.log(studentAccIds[0].studentAccounts)
+    // console.log(studentAccIds)
+    // console.log(studentAccIds[0].studentAccounts)
 
-        if (semesters.length > 0) {
-            const studentIDs = await StudentAccount
-                .find({})
-                .where('_id').in(semesters[0].studentAccounts)
-                .lean()
-
-            // console.log(studentIDs.map(x => x.student_id))
-
-            const students = await Student
-                .find({})
-                .where('_id').in(studentIDs.map(x => x.student_id))
-                .lean()
-
-            console.log(students)
-
-
-            const individuals = await Individual
-                .find({})
-                .where('_id').in(students.map(x => x.indID))
-                .lean()
-
-            return <main className="two-col">
-                <div className="items">
-                    {individuals.map((x, idx) => {
-                        return <a key={idx} href={"/dashboard/" + budgetID + "/Rates/" + x._id.toString()}>{x.name}</a>
-                    })}
-                    <button>Add New Semester</button>
-                </div>
-                <div>
-                    {children}
-                </div>
-            </main>
-        }
+    if (semesters.length > 0) {
+        return <main className="two-col">
+            <div className="items">
+                {semesters.map(x => {
+                    return <Link href={`/dashboard/${budgetID}/Rates/${x._id.toJSON()}`} key={x._id.toJSON()}>{x.semester}</Link>
+                })}
+                <Link href={`/dashboard/${budgetID}/Rates/`}>Add New Semester</Link>
+            </div>
+            <div>
+                {children}
+            </div>
+        </main>
+    }
 
         // console.log(individuals)
 
     return <main className="two-col">
         <div className="items">
-            <button>Add New Semester</button>
+            <Link href={`/dashboard/${budgetID}/Rates/`}>Add New Semester</Link>
         </div>
         <div>
             {children}
