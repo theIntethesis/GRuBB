69ec4857e78db72ee9d3137249a4d40bd5e9498f
theIntethesis <hayden@gnorth.net>
2025-11-19 20:42:13 -0800

    Most of the form sections done.

--- src/forms/individualForms.tsx
+++ src/forms/individualForms.tsx
@@ -42,7 +42,6 @@ function FacultyLine({faculty}: {faculty?: any}) {
     useEffect(() => {
         console.log(faculty?.role || "Faculty")
         setRole(faculty?.role || "Faculty")
-
     }, [faculty])
 
     return <tr>
@@ -59,7 +58,49 @@ function FacultyLine({faculty}: {faculty?: any}) {
 
 function SalaryAccountSection({salaryAccount}: {salaryAccount?: any}) {
     // salary stuff
-    return<></>
+    const calculatePayment = () => {
+        const rate = (document.getElementById("rate") as HTMLInputElement).valueAsNumber;
+        const rateUnit = (document.getElementById("rateUnit") as HTMLSelectElement).value;
+        const percentFTE = (document.getElementById("percentFTE") as HTMLInputElement).valueAsNumber;
+        console.log("Rate:", rate, "Rate Unit:", rateUnit, "Percent FTE:", percentFTE);
+        (document.getElementById("payment") as HTMLInputElement).value = (Number(rate) * (rateUnit === "Hour" ? 2080 : 1) * (Number(percentFTE) / 100)).toString()
+    }
+    
+    return <>
+        <tr>
+            <td>
+                <label htmlFor="rate">Rate:</label>
+            </td>
+            <td>
+                $<input type="float" id="rate" name="rate" onChange={calculatePayment}defaultValue={salaryAccount != null ? salaryAccount.rate : 0}/>  / <select id="rateUnit" onChange={calculatePayment} defaultValue={salaryAccount != null ? salaryAccount.rate : "Hour"}><option>Hour</option><option>Year</option></select>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label htmlFor="percentFTE">Percent of Full Time Equivalent:</label>
+            </td>
+            <td>
+                <input type="number" id="percentFTE" name="percentFTE" min={0} max={100} onChange={calculatePayment} defaultValue={salaryAccount != null ? salaryAccount.percentFTE * 100 : 100}/>%
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label htmlFor="payment">Payment:</label>
+            </td>
+            <td>
+                $<input type="number" id="payment" name="payment" disabled={true} defaultValue={salaryAccount != null ? salaryAccount.rate * (salaryAccount.rateUnit === "Hour" ? 2080 : 1) * salaryAccount.percentFTE : 0}/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label htmlFor="FBR">Fringe Benefits Rate:</label>
+            </td>
+            <td>
+                {/* This needs to be updated when the Faculty type is updated. */}
+                <input type="number" id="FBR" name="FBR" min={0} max={100} disabled={true}/>%
+            </td>
+        </tr>
+    </>
 }
 
 function OptionalSalaryAccountSection({salaryAccount}: {salaryAccount?: any}) {
@@ -70,7 +111,24 @@ function OptionalSalaryAccountSection({salaryAccount}: {salaryAccount?: any}) {
 
 function StudentAccountSection({studentAccount}: {studentAccount?: any}) {
     // student account stuff
-    return<></>
+    return <>
+        <tr>
+            <td>
+                <label htmlFor="tuition">Tuition:</label>
+            </td>
+            <td>
+                $<input type="number" id="tuition" name="tuition" defaultValue={studentAccount != null ? studentAccount.tuition : 0}/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label htmlFor="aid">Aid Received:</label>
+            </td>
+            <td>
+                $<input type="number" id="aid" name="aid" defaultValue={studentAccount != null ? studentAccount.aidReceived : 0}/>
+            </td>
+        </tr>
+    </>
 }
 
 // individual == null implies everything else is null
@@ -92,6 +150,8 @@ export function StudentForm(
             modifyStudent(
                 formData.get("name")?.toString() || student.name,
                 formData.get("outOfState") == "on",
+                // formData.get("tuition"), 
+                // formData.get("aid"),
                 student.individual_id
             )
         }
@@ -113,8 +173,8 @@ export function StudentForm(
                 <StudentAccountSection/>
                 <OptionalSalaryAccountSection/>
                 <tr>
-                    {student != null ? <td colSpan={2} style={{width: "100%"}}><button formAction={onDelete}>Delete</button></td> : undefined}
-                    <td colSpan={2} style={{width: "100%"}}><button>Submit</button></td>
+                    <td><button style={{width: "100%"}}>Submit</button></td>
+                    {student != null ? <td><button formAction={onDelete} style={{width: "100%"}}>Delete</button></td> : undefined}
                 </tr>
 
             </tbody>
@@ -143,6 +203,8 @@ export function FacultyForm(
             modifyFaculty(
                 formData.get("name")?.toString() || faculty.name,
                 formData.get("facultyType")?.toString() || "Faculty",
+                // formData.get("rate"),
+                // formData.get("percentFTE"),
                 faculty.individual_id
             )
         }
@@ -161,8 +223,8 @@ export function FacultyForm(
                  {/* semester switcher, uses redirects */}
                  <SalaryAccountSection/>
                  <tr>
-                    {faculty != null ? <td><button formAction={onDelete}>Delete</button></td> : undefined}
-                    <td><button>Submit</button></td>
+                    <td><button style={{width: "100%"}}>Submit</button></td>
+                    {faculty != null ? <td><button formAction={onDelete} style={{width: "100%"}}>Delete</button></td> : undefined}
                 </tr>
             </tbody>
         </table>
